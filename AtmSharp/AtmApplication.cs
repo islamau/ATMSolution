835f563449583ba5cec52a50ed45566b69e36d2d
Prof. M <magdin.stoica@sheridancollege.ca>
2017-01-30 09:42:41 -0500

    Add project files.

---
+++ AtmSharp/AtmApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmSharp
+{
+    /// <summary>
+    /// The main class of the program that defines the program entry point, creates the ATM and the bank
+    /// and starts the ATM
+    /// </summary>
+    class AtmApplication
+    {
+        /// <summary>
+        /// The main method that starts the program. The args parameter that Visual Studio generates was
+        /// removed as it is not used. C# allows the Main() method not to have an args parameter
+        /// </summary>
+        static void Main()
+        {
+        }
+    }
+}
